0eada6d08768b0f8ef883ea086ffd881b19456ed
Ethan Seulin <ethan.seulin@gmail.com>
2025-10-30 16:35:02 +0100

    menu déroulant fermer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    const buttonFolder = document.getElementById('folder-button');
+    const folder = document.getElementsByClassName('folder-items')[0];
+
+    buttonFolder.addEventListener('click', () => {
+        folder.classList.toggle('closed');
+        buttonFolder.classList.toggle('closed');
+    })
+
     const elementsToAnimate = document.querySelectorAll(
         '.footer-image, .timeline-container'
     );
@@ -6,22 +15,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if(entry.isIntersecting) {
-
                 if(entry.target.classList.contains('footer-image')) {
                     entry.target.classList.add('visible');
                 }
-
                 if(entry.target.classList.contains('timeline-container')) {
                     entry.target.classList.add('visible');
                     observer.unobserve(entry.target);
                 }
-
             } else {
-
                 if(entry.target.classList.contains('footer-image')) {
                     entry.target.classList.remove('visible');
                 }
-
             }
         });
     });
